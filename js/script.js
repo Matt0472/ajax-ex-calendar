354eb10b2979e459dd077be25e203d27cf9a9bc1
Mattia Pedone <mattiapedone93@gmail.com>
2020-02-03 16:40:08 +0100

    aggiunto il metodo per l'anno bisestile e il numero di giorni nel mese, più la chiamata ajax

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
 $(document).ready(function() {
-  
+  var tempo = moment("2018-01", "YYYY-MM").daysInMonth();
+  console.log(tempo);
+  var bisestile = moment([2018]).isLeapYear();
+  console.log(bisestile);
+
+  $.ajax(
+      {
+        url: 'https://flynn.boolean.careers/exercises/api/holidays?year=2018&month=0',
+        method: "GET",
+        success: function(data, stato) {
+          console.log(data.response);
+        },
+        error: function(richiesta, stato, errori) {
+          alert('E\' avvenuto un errore.' + errori);
+        }
+      }
+    );
 });
